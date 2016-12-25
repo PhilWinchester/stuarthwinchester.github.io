a6b517095040d21f613249d6ca5d990a551bad45
Philip Winchester <philipwinc@gmail.com>
2016-12-25 10:03:55 -0500

    adding landing page styling

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict';
 
 (() => {
-  console.log('script loaded');
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date()
+    }
+  })
 })();
